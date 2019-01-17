3bcb12ceb160714b8aa5012a470c3ba9e04f529b
colesirokman <46791310+colesirokman@users.noreply.github.com>
2019-01-17 11:34:44 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include<studio.h>
 #include<stdlib.h>
 int main(int argc, char **v){
-  printf("Cole Sirokman");
+  printf("Cole Sirokman\n");
+  printf("Electrical Engineering\n");
   return 0;
 }
